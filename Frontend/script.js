64f000857909582b318d65da2cea7eb055646ad1
Flamerman <vasiagavryluk20@gmail.com>
2024-02-16 08:39:52 +0200

    Some changes

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const form = document.querySelector('form');
 
+    console.log(form);
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
 
