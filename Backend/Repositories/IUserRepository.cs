50a87c3f2bbce26683689721c3a5d1b3c1d9235b
Flamerman <vasiagavryluk20@gmail.com>
2024-02-25 12:14:40 +0200

    Changes

---
+++ Backend/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Models;
+
+namespace Backend.Repositories
+{
+    interface IUserRepository
+    {
+        List<User> getAll();
+        User getById(int id);
+        void save(User user);
+        void deleteById(int id);
+        void update(User user);
+    }
+}
