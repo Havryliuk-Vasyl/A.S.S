0d9b5c8d4c93c1dbe2b3e9445acf551b23e34f49
Flamerman <vasiagavryluk20@gmail.com>
2024-02-29 12:09:09 +0200

    Great Changes

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+754ea76b0ed68addf87cb22801718fdefdc734f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4da9d4bd0be0e16cc2a5f6ed71e8ba97779fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
