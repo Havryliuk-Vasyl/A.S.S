c3c6997fde0db905193dc702d50741e78467f130
Flamerman <vasiagavryluk20@gmail.com>
2024-04-02 13:21:50 +0300

    Changes

--- Backend/Models/ApplicationDbContext.cs
+++ Backend/Models/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace Backend.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<UserModel> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
