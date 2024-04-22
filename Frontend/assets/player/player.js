a71c3634dfd3e2a97172a1ae41a5eab5da2986e8
Flamerman <vasiagavryluk20@gmail.com>
2024-04-22 22:35:12 +0300

    Great Changes

--- Frontend/assets/player/player.js
+++ Frontend/assets/player/player.js
@@ -1,58 +1,229 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const audioPlayer = document.getElementById('audioPlayer');
-    const playPauseBtn = document.getElementById('playPauseBtn');
-    const volumeSlider = document.getElementById('volumeSlider');
-    const timeSlider = document.getElementById('timeSlider');
-    const currentTimeDisplay = document.getElementById('currentTime');
-    const durationDisplay = document.getElementById('duration');
+class Player{
+    constructor(){}
 
-    playPauseBtn.addEventListener('click', function() {
-        if (audioPlayer.paused) {
-            audioPlayer.play();
+    renderPlayer(){
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = "../assets/player/player.css";
+        document.head.appendChild(link);
+    
+        var playerDiv = document.createElement("div");
+        playerDiv.classList.add("player");
+    
+        var audioPlayerDiv = document.createElement("div");
+        audioPlayerDiv.classList.add("audio-player");
+    
+        var controlsDiv = document.createElement("div");
+        controlsDiv.classList.add("controls");
+    
+        var songInformationDiv = document.createElement("div");
+        songInformationDiv.classList.add("song-information");
+    
+        var songImageDiv = document.createElement("div");
+        songImageDiv.classList.add("song-image");
+        var songImage = document.createElement("img");
+        songImage.src = "";
+        songImage.alt = "";
+        songImageDiv.appendChild(songImage);
+    
+        var songNameDiv = document.createElement("div");
+        songNameDiv.classList.add("song-name");
+        songNameDiv.id = "song-name";
+        var songNameLink = document.createElement("a");
+        songNameLink.href = "#";
+        songNameLink.textContent = "No name";
+        songNameLink.id = "songName";
+        songNameDiv.appendChild(songNameLink);
+    
+        var artistNameDiv = document.createElement("div");
+        artistNameDiv.classList.add("artist-name");
+        artistNameDiv.id = "artist-name";
+        var artistNameLink = document.createElement("a");
+        artistNameLink.href = "#";
+        artistNameLink.textContent = "No artist";
+        artistNameLink.id = "songArtist";
+        artistNameDiv.appendChild(artistNameLink);
+    
+        songInformationDiv.appendChild(songImageDiv);
+        songInformationDiv.appendChild(songNameDiv);
+        songInformationDiv.appendChild(artistNameDiv);
+    
+        var audioControlDiv = document.createElement("div");
+        audioControlDiv.classList.add("audio-control");
+    
+        var controlButtonsDiv = document.createElement("div");
+        controlButtonsDiv.classList.add("control-buttons");
+        var prebSongBtn = document.createElement("button");
+        prebSongBtn.id = "prebSongBtn";
+        prebSongBtn.innerHTML = "|&lt;";
+        var playPauseBtn = document.createElement("button");
+        playPauseBtn.id = "playPauseBtn";
+        playPauseBtn.innerHTML = "&gt;";
+        var nextSongBtn = document.createElement("button");
+        nextSongBtn.id = "nextSongBtn";
+        nextSongBtn.innerHTML = "&gt;|";
+        controlButtonsDiv.appendChild(prebSongBtn);
+        controlButtonsDiv.appendChild(playPauseBtn);
+        controlButtonsDiv.appendChild(nextSongBtn);
+    
+        var timeControlDiv = document.createElement("div");
+        timeControlDiv.classList.add("time-control");
+        var currentTimeDiv = document.createElement("div");
+        currentTimeDiv.id = "currentTime";
+        currentTimeDiv.textContent = "0:00";
+        var timeSliderInput = document.createElement("input");
+        timeSliderInput.type = "range";
+        timeSliderInput.id = "timeSlider";
+        timeSliderInput.min = "0";
+        timeSliderInput.max = "1";
+        timeSliderInput.step = "0.001";
+        timeSliderInput.value = "0";
+        var durationDiv = document.createElement("div");
+        durationDiv.id = "duration";
+        durationDiv.textContent = "0:00";
+        timeControlDiv.appendChild(currentTimeDiv);
+        timeControlDiv.appendChild(timeSliderInput);
+        timeControlDiv.appendChild(durationDiv);
+    
+        audioControlDiv.appendChild(controlButtonsDiv);
+        audioControlDiv.appendChild(timeControlDiv);
+    
+        var volumeController = document.createElement("div");
+    
+        var volumeSliderInput = document.createElement("input");
+        volumeSliderInput.type = "range";
+        volumeSliderInput.id = "volumeSlider";
+        volumeSliderInput.min = "0";
+        volumeSliderInput.max = "1";
+        volumeSliderInput.step = "0.1";
+        volumeSliderInput.value = "1";
+    
+        volumeController.appendChild(volumeSliderInput);
+    
+        controlsDiv.appendChild(songInformationDiv);
+        controlsDiv.appendChild(audioControlDiv);
+        controlsDiv.appendChild(volumeController);
+    
+        audioPlayerDiv.appendChild(controlsDiv);
+    
+        var audioPlayer = document.createElement("audio");
+        audioPlayer.id = "audioPlayer";
+    
+        playerDiv.appendChild(audioPlayerDiv);
+        playerDiv.appendChild(audioPlayer);
+    
+        document.body.appendChild(playerDiv);
+    
+        playPauseBtn.addEventListener('click', function() {
+            if (audioPlayer.paused) {
+                audioPlayer.play();
+                playPauseBtn.innerHTML = '| |';
+                console.log(audioPlayer);
+            } else {
+                audioPlayer.pause();
+                playPauseBtn.innerHTML = '&gt;';
+                console.log(audioPlayer);
+            }
+        });
+    
+        volumeSliderInput.addEventListener('input', function() {
+            audioPlayer.volume = volumeSliderInput.value;
+        });
+    
+        audioPlayer.addEventListener('timeupdate', function() {
+            const currentTime = audioPlayer.currentTime;
+            const duration = audioPlayer.duration;
+    
+            timeSliderInput.value = currentTime / duration;
+    
+            currentTimeDiv.textContent = formatTime(currentTime);
+            durationDiv.textContent = formatTime(duration);
+        });
+    
+        let isSeeking = false;
+    
+        timeSliderInput.addEventListener('input', function() {
+            isSeeking = true;
+            const duration = audioPlayer.duration;
+            const seekTo = duration * timeSliderInput.value;
+    
+            audioPlayer.currentTime = seekTo;
+        });
+    
+        timeSliderInput.addEventListener('mouseup', function() {
+            isSeeking = false; 
+        });
+    
+        audioPlayer.addEventListener('timeupdate', function() {
+            const currentTime = audioPlayer.currentTime;
+            const duration = audioPlayer.duration;
+            if (!isSeeking) {
+                timeSliderInput.value = currentTime / duration;
+            }
+            currentTimeDiv.textContent = formatTime(currentTime);
+            durationDiv.textContent = formatTime(duration);
+        });
+    
+        audioPlayer.addEventListener('play', function(){
             playPauseBtn.innerHTML = '| |';
-        } else {
-            audioPlayer.pause();
-            playPauseBtn.innerHTML = '&gt;';
-        }
-    });
+        });
+    
+        document.addEventListener('keydown', function(event) {
+            if (event.code === 'Space') {
+                if (audioPlayer.paused) {
+                    audioPlayer.play();
+                    playPauseBtn.innerHTML = '| |';
+                } else {
+                    audioPlayer.pause();
+                    playPauseBtn.innerHTML = '&gt;';
+                }
+            }
+        });
+    }
 
-    volumeSlider.addEventListener('input', function() {
-        audioPlayer.volume = volumeSlider.value;
-    });
+    play(songId){
+        $.ajax({
+            type: "GET",
+            url: "https://localhost:7219/Song/id?id=" + songId,
+            success: (response) => {
+                console.log("Song:", response);
+                this.playSong(response.song.id)
+    
+                document.getElementById("songName").textContent = response.song.title;
+                document.getElementById("songArtist").textContent = response.artistName;
+            },
+            error: (error) => {
+                console.error("Помилка при отриманні списку пісень:", error);
+            }
+        });
+    }
+    
+    playSong(songId){
+        console.log(songId + " is not playing!");
+        const apiUrl = `https://localhost:7219/Audio/${songId}`;
+        axios.get(apiUrl, {
+                responseType: 'arraybuffer'
+            })
+            .then(response => {
+                const audioData = response.data;
+                const audioUrl = URL.createObjectURL(new Blob([audioData]));
+            
+                const audioElement = document.getElementById('audioPlayer')
+                audioElement.src = audioUrl;
+                
+                audioElement.play();
+            })
+            .catch(error => {
+                console.error('Error fetching audio file:', error);
+            });
+    }
+}
 
-    audioPlayer.addEventListener('timeupdate', function() {
-        const currentTime = audioPlayer.currentTime;
-        const duration = audioPlayer.duration;
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
 
-        // Оновлення регулятора часу
-        timeSlider.value = currentTime / duration;
-
-        // Оновлення відображення часу
-        currentTimeDisplay.textContent = formatTime(currentTime);
-        durationDisplay.textContent = formatTime(duration);
-    });
-
-    let isSeeking = false;
-
-    timeSlider.addEventListener('input', function() {
-        isSeeking = true;
-        const duration = audioPlayer.duration;
-        const seekTo = duration * timeSlider.value;
-
-        audioPlayer.currentTime = seekTo;
-    });
-
-    timeSlider.addEventListener('mouseup', function() {
-        isSeeking = false; 
-    });
-
-    audioPlayer.addEventListener('timeupdate', function() {
-        const currentTime = audioPlayer.currentTime;
-        const duration = audioPlayer.duration;
-        if (!isSeeking) {
-            timeSlider.value = currentTime / duration;
-        }
-        currentTimeDisplay.textContent = formatTime(currentTime);
-        durationDisplay.textContent = formatTime(duration);
-    });
-});
+export default Player;
--- Frontend/assets/scripts/catalog.js
+++ Frontend/assets/scripts/catalog.js
@@ -1,45 +1,71 @@
-var songsContainer = document.createElement("div");
-    songsContainer.id = "songs-container";
-    songsContainer.classList.add("container-style"); // Додайте клас для стилізації
+import Player from '../player/player.js';
 
-    $.ajax({
-        type: "GET",
-        url: "https://localhost:7219/Song",
-        success: function(response) {
-            console.log("Songs:", response);
-            renderRecentSongs(response);
-        },
-        error: function(error) {
-            console.error("Помилка при отриманні списку пісень:", error);
-        }
-    })
+class Catalog {
+    constructor(){
+        this.player = new Player();
+    }
 
-    function renderRecentSongs(songs) {
-        songs.forEach(function(item) {
+    includeStyles(){
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = "../assets/styles/catalog.css";
+        document.head.appendChild(link);
+    }
+
+    getAudioList(){
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: "GET",
+                url: "https://localhost:7219/Song",
+                success: function(response) {
+                    console.log("Songs:", response);
+                    resolve(response);
+                },
+                error: function(error) {
+                    console.error("Помилка при отриманні списку пісень:", error);
+                    reject(error);
+                }
+            });
+        });
+    }
+    async renderRecentSongs() {
+        this.includeStyles();
+        try {
+            const songs = await this.getAudioList();
+            console.log(songs);
+
+            var songsContainer = document.createElement("div");
+            songsContainer.id = "songs-container";
+            songsContainer.classList.add("container-style");
+
+            console.log(songs);
+            const self = this;
+            songs.forEach(function(item) {
             var songDiv = document.createElement("div");
-            songDiv.classList.add("song");
+            songDiv.classList.add("catalog-song");
             songDiv.setAttribute("data-id", item.song.id);
 
             var songInformationDiv = document.createElement("div");
-            songInformationDiv.classList.add("song-information");
+            songInformationDiv.classList.add("catalog-song-information");
 
             var songPhotoDiv = document.createElement("div");
-            songPhotoDiv.classList.add("song-photo");
+            songPhotoDiv.classList.add("catalog-song-photo");
             var songPhotoImg = document.createElement("img");
-            songPhotoImg.src = "https://localhost:7219/Song/photo/" + item.photo.id; 
+            songPhotoImg.src = "https://localhost:7219/Song/photo/" + item.photo.id;
             songPhotoImg.alt = "Photo";
             songPhotoImg.style.maxWidth = "50px";
-            songPhotoImg.style.maxHeight = "50px"; 
+            songPhotoImg.style.maxHeight = "50px";
             songPhotoImg.style.width = "100%";
             songPhotoImg.style.height = "100%";
             songPhotoDiv.appendChild(songPhotoImg);
 
             var songNameDiv = document.createElement("div");
-            songNameDiv.classList.add("song-name");
+            songNameDiv.classList.add("catalog-song-name");
             songNameDiv.textContent = item.song.title;
 
             var artistDiv = document.createElement("div");
-            artistDiv.classList.add("artist");
+            artistDiv.classList.add("catalog-artist");
             artistDiv.textContent = item.artist;
 
             songInformationDiv.appendChild(songPhotoDiv);
@@ -47,31 +73,18 @@ var songsContainer = document.createElement("div");
             songInformationDiv.appendChild(artistDiv);
 
             var songPlayControlDiv = document.createElement("div");
-            songPlayControlDiv.classList.add("song-play-control");
+            songPlayControlDiv.classList.add("catalog-song-play-control");
 
             var durationDiv = document.createElement("div");
-            durationDiv.classList.add("duration");
+            durationDiv.classList.add("catalog-duration");
             durationDiv.textContent = formatDuration(item.duration);
 
             var playButtonDiv = document.createElement("div");
-            playButtonDiv.classList.add("play-button");
+            playButtonDiv.classList.add("catalog-play-button");
             playButtonDiv.textContent = ">";
 
-            playButtonDiv.addEventListener("click", function() {
-                var songId = item.song.id;
-                $.ajax({
-                    type: "GET",
-                    url: "https://localhost:7219/audio/" + songId,
-                    success: function(audioData) {
-                        var audio = new Audio();
-                        audio.src = URL.createObjectURL(new Blob([audioData], { type: "audio/mpeg" }));
-
-                        audio.play();
-                    },
-                    error: function(error) {
-                        console.error("Помилка при отриманні аудіоданих пісні:", error);
-                    }
-                });
+            playButtonDiv.addEventListener("click", () => {
+                self.player.play(item.song.id);
             });
 
             songPlayControlDiv.appendChild(durationDiv);
@@ -80,25 +93,34 @@ var songsContainer = document.createElement("div");
             songDiv.appendChild(songInformationDiv);
             songDiv.appendChild(songPlayControlDiv);
 
-            songsContainer.appendChild(songDiv);
+            songDiv.addEventListener('dblclick', () => {
+                self.player.play(item.song.id);
+            });
 
+            songsContainer.appendChild(songDiv);
+            });
             document.getElementById("catalog").appendChild(songsContainer);
-        });
-    }
-    
-    function formatDuration(durationInSeconds) {
-        var hours = Math.floor(durationInSeconds / 3600);
-        var minutes = Math.floor((durationInSeconds % 3600) / 60);
-        var seconds = Math.floor(durationInSeconds % 60);
-    
-        var formattedDuration = "";
-    
-        if (hours > 0) {
-            formattedDuration += hours + ":";
+        } catch (error) {
+            console.error("Помилка під час відображення пісень:", error);
         }
-    
-        formattedDuration += (minutes < 10 ? "0" : "") + minutes + ":";
-        formattedDuration += (seconds < 10 ? "0" : "") + seconds;
-    
-        return formattedDuration;
+    }  
+}
+
+function formatDuration(durationInSeconds) {
+    var hours = Math.floor(durationInSeconds / 3600);
+    var minutes = Math.floor((durationInSeconds % 3600) / 60);
+    var seconds = Math.floor(durationInSeconds % 60);
+
+    var formattedDuration = "";
+
+    if (hours > 0) {
+        formattedDuration += hours + ":";
     }
+
+    formattedDuration += (minutes < 10 ? "0" : "") + minutes + ":";
+    formattedDuration += (seconds < 10 ? "0" : "") + seconds;
+
+    return formattedDuration;
+}
+
+export default Catalog;
